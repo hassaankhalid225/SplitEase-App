0e0159cd5fe345a2e55241294480c63aad034088
Hassaan khalid <hassaankhalid225@gmail.com>
2026-05-10 15:01:36 +0500

    feat: implement initial session management flow and project build configuration

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,18 +19,6 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
-subprojects {
-    val project = this
-    project.plugins.withId("com.android.library") {
-        val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
-        android.compileSdkVersion(34)
-    }
-    project.plugins.withId("com.android.application") {
-        val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
-        android.compileSdkVersion(34)
-    }
-}
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
